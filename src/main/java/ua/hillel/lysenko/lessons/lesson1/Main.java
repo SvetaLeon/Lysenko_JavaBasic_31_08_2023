b1fe9b4dfb2a518c3763002d6de8a02bb05dd732
Lysenko Svetlana <svetalysenko25@gmail.com>
2023-09-09 16:49:01 +0300

    commit all files

---
+++ src/main/java/ua/hillel/lysenko/lessons/lesson1/Main.java
@@ -0,0 +1,7 @@
+package ua.hillel.lysenko.lessons.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/ua/hillel/lysenko/lessons/lesson2/MyApp.java
@@ -0,0 +1,12 @@
+package ua.hillel.lysenko.lessons.lesson2;
+
+public class MyApp {
+    public static void main(String[] args) {
+        double latitude = 25.1916159002;
+        double longitude = 55.2711322488;
+        char specSymbol1 = '°';
+        char specSymbol2 = '\'';
+        System.out.println("Latitude " + latitude + " Longitude " + longitude);
+        System.out.println("\"DMS Lat\": 25" + specSymbol1 + " 11" + specSymbol2 + " 29.82\" N DMS Long: 55" + specSymbol1 + " 16' 16.08\" E");
+    }
+}
