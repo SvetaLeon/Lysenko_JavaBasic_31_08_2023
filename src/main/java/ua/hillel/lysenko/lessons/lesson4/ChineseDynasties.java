8fd0e473c2560b494b373d29f6da7507e647fd30
Lysenko Svetlana <svetalysenko25@gmail.com>
2023-09-16 17:48:27 +0300

    added homework Chinese dynasties

---
+++ src/main/java/ua/hillel/lysenko/lessons/lesson4/ChineseDynasties.java
@@ -0,0 +1,26 @@
+package ua.hillel.lysenko.lessons.lesson4;
+
+public class ChineseDynasties {
+    public static void main(String[] args) {
+        int attackPowerWarriorLi = 13;
+        int attackPowerBowmanLi = 24;
+        int attackPowerRiderLi = 46;
+
+        int numberWarriorsEachTypeLi = 860;
+
+        int totalAttackPowerLi = (attackPowerWarriorLi * numberWarriorsEachTypeLi) + (attackPowerBowmanLi * numberWarriorsEachTypeLi) + (attackPowerRiderLi * numberWarriorsEachTypeLi);
+
+        System.out.println("The total attack power of the Li Dynasty = " + totalAttackPowerLi);
+
+        int attackPowerWarriorMing = 9;
+        int attackPowerBowmanMing = 35;
+        int attackPowerRiderMing = 12;
+
+        double percentage = 1.5;
+        int numberWarriorsEachTypeMing = (int) (numberWarriorsEachTypeLi * percentage);
+
+        int totalAttackPowerMing = (attackPowerWarriorMing * numberWarriorsEachTypeMing) + (attackPowerBowmanMing * numberWarriorsEachTypeMing) + (attackPowerRiderMing * numberWarriorsEachTypeMing);
+
+        System.out.println("The total attack power of the Ming Dynasty = " + totalAttackPowerMing);
+    }
+}
