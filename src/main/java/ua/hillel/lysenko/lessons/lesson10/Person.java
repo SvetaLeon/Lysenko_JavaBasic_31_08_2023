7da0c8289b30fcebd6f20205564b6dcfc306dc49
Lysenko Svetlana <svetalysenko25@gmail.com>
2023-10-09 20:15:10 +0300

    added homework method personInfo()

--- src/main/java/ua/hillel/lysenko/lessons/lesson10/Person.java
+++ src/main/java/ua/hillel/lysenko/lessons/lesson10/Person.java
@@ -3,13 +3,16 @@ package ua.hillel.lysenko.lessons.lesson10;
 public class Person {
     public static void main(String[] args) {
 
-        personInfo("Will", "Smith", "New York", "2936729462846");
-        personInfo("Jackie", "Chan", "Shanghai", "12312412412");
-        personInfo("Sherlock", "Holmes", "London", "37742123513");
+        String person1 = personInfo("Will", "Smith", "New York", "2936729462846");
+        String person2 = personInfo("Jackie", "Chan", "Shanghai", "12312412412");
+        String person3 = personInfo("Sherlock", "Holmes", "London", "37742123513");
 
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
     }
 
-    static void personInfo(String name, String surname, String city, String phone) {
-        System.out.println("Call a citizen " + name + " " + surname + " from city " + city + " you can by number " + phone + ".");
+    static String personInfo(String name, String surname, String city, String phone) {
+        return "Call a citizen " + name + " " + surname + " from city " + city + " you can by number " + phone + ".";
     }
 }
