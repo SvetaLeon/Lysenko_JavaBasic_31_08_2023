7b6a80e3861a1ab8eeec96362ab14abc5acd965d
Shutko Svetlana <svetalysenko25@gmail.com>
2023-10-25 19:54:50 +0300

    added homework15

--- src/main/java/ua/hillel/shutko/homework/homework15/Drinks.java
+++ src/main/java/ua/hillel/shutko/homework/homework15/Drinks.java
@@ -1,5 +1,8 @@
 package ua.hillel.shutko.homework.homework15;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Drinks {
     public static final int COST_COFFEE = 35;
     public static final int COST_TEA = 30;
@@ -10,9 +13,101 @@ public class Drinks {
 
     public static int COUNTER_DRINKS = 0;
 
+    public static int totalCost = 0;
+
     public Drinks() {
         COUNTER_DRINKS++;
     }
 
+    public static void main(String[] args) {
+        makingDrink();
+        System.out.println("The number of manufactured drinks: " + COUNTER_DRINKS + ".");
+        System.out.println("The total amount to be paid is " + totalCost + " UAH.");
+    }
+
+    static void makingDrink() {
+
+        Scanner scanner = new Scanner(System.in);
+        DrinksMachine drinksMachine = null;
+        int orderMore = 1;
+        int costDrink = 0;
+        DrinksMachine[] drinksMachines = DrinksMachine.values();
+
+        while (orderMore == 1) {
+
+            while (drinksMachine == null) {
+                System.out.println("Please enter the type of drink from the list: " + Arrays.toString(drinksMachines));
+                String userDrink = scanner.nextLine().toUpperCase();
+
+                for (DrinksMachine drinks : drinksMachines) {
+                    if (userDrink.equals(drinks.toString())) {
+                        drinksMachine = DrinksMachine.valueOf(userDrink);
+                        break;
+                    }
+                }
+            }
+
+            switch (drinksMachine) {
+                case COFFEE: {
+                    System.out.println("The drink " + DrinksMachine.COFFEE + " is ready.");
+                    costDrink = COST_COFFEE;
+                    break;
+                }
+                case TEA: {
+                    System.out.println("The drink " + DrinksMachine.TEA + " is ready.");
+                    costDrink = COST_TEA;
+                    break;
+                }
+                case LEMONADE: {
+                    System.out.println("The drink " + DrinksMachine.LEMONADE + " is ready.");
+                    costDrink = COST_LEMONADE;
+                    break;
+                }
+                case MOJITO: {
+                    System.out.println("The drink " + DrinksMachine.MOJITO + " is ready.");
+                    costDrink = COST_MOJITO;
+                    break;
+                }
+                case MINERAL_WATER: {
+                    System.out.println("The drink " + DrinksMachine.MINERAL_WATER + " is ready.");
+                    costDrink = COST_MINERAL_WATER;
+                    break;
+                }
+                case COCA_COLA: {
+                    System.out.println("The drink " + DrinksMachine.COCA_COLA + " is ready.");
+                    costDrink = COST_COCA_COLA;
+                    break;
+                }
+            }
+
+            while (true) {
+                System.out.println("Would you like to order something else? If Yes, enter 1, if No, enter 2.");
+
+                if (scanner.hasNextInt()) {
+                    orderMore = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (orderMore >= 1 && orderMore <= 2) {
+                        break;
+                    } else {
+                        System.out.println("Please enter number from 1 or 2");
+                    }
+
+                } else {
+                    System.out.println("Please enter correct value!!!");
+                    scanner.nextLine();
+                }
+            }
+
+            totalCost += costDrink;
+            drinksMachine = null;
+
+            if (totalCost > 0) {
+                new Drinks();
+            } else {
+                System.out.println("You need to order something");
+            }
+        }
+    }
 }
 
---
+++ src/main/java/ua/hillel/shutko/lessons/lesson2/Main.java
@@ -0,0 +1,110 @@
+package ua.hillel.shutko.lessons.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        byte agePerson;
+//        agePerson = 15;
+//
+//        System.out.println(agePerson);
+//
+//        agePerson = 22;
+//
+//        System.out.println(agePerson);
+
+//        byte agePerson = 22;
+//        agePerson = 55;
+//
+//        byte ageCopy = agePerson;
+//
+//        System.out.println(agePerson);
+//        System.out.println(ageCopy);
+//
+//        ageCopy = 77;
+//
+//        System.out.println();
+//        System.out.println(agePerson);
+//        System.out.println(ageCopy);
+
+
+//        short shortVariable = 32111;
+//        int intVariable = 2147000000;
+//        long longVariable = 12341231231234111L;
+
+
+//        float floatVariable = 11.1111111111111111F;
+//        double doubleVariable = 11.1111111111111111;
+//
+//        System.out.println(floatVariable);
+//        System.out.println(doubleVariable);
+
+
+
+        char chA = 'A';
+        char chA2 = 65;
+        char chA3 = '\u0041';
+
+        System.out.println(chA);
+        System.out.println(chA2);
+        System.out.println(chA3);
+
+        System.out.println('\u004F');
+        System.out.println('\u26A1');
+        System.out.println('\u00B0');
+//
+//        System.out.println("Й");
+
+
+//        boolean isCar = false;
+//        boolean isNext = true;
+
+
+
+//        String str1 = "Hello world";
+//        String str2 = "Hello world";
+//        String str3 = new String("Hello world");
+//
+//
+//
+//        str1 = str1 + " Java Best " + "12";
+//
+//        System.out.println(str1);
+
+
+
+//        byte number = 5;
+//
+//        int intNumber = number;
+//
+//        double doubleNumber = number;
+//
+//        System.out.println(doubleNumber);
+
+
+//        char chA = '^';
+//
+//        int intA = chA;
+//
+//        System.out.println(intA);
+
+
+
+//        int intNumber = 345634563;
+//
+//        byte byteNumber = (byte) intNumber;
+//
+//        System.out.println(byteNumber);
+
+
+//        double doubleVariable = 15.676;
+//        int intVariable = (int) doubleVariable;
+//
+//        System.out.println(intVariable);
+
+
+//        int number = 5;
+//        String str = (String) number; // ERROR!!!!
+
+//        System.out.println("qwe" + 1 + 1);
+//        System.out.println(1 + 1 + "qwe");
+    }
+}
